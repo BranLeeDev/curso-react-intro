37abb4c969eef077d67787a28afc1ef9c9d8a8cc
Brandon <brandonaguerodeveloper@gmail.com>
2024-03-04 07:34:36 -0500

    class seven

--- src/components/TodoSearch.js
+++ src/components/TodoSearch.js
@@ -1,8 +1,19 @@
+import React from "react";
 import "../styles/TodoSearch.css";
 
 const TodoSearch = () => {
+  const [searchValue, setSearchValue] = React.useState("");
+
   return (
-    <input className="todo-search" type="text" placeholder="Cortar cebolla" />
+    <input
+      className="todo-search"
+      value={searchValue}
+      onChange={(event) => {
+        setSearchValue(event.target.value);
+      }}
+      type="text"
+      placeholder="Cortar cebolla"
+    />
   );
 };
 
