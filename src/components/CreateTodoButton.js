332a70dc8474edd39d20887bb117946b716a7afa
Brandon <brandonaguerodeveloper@gmail.com>
2024-03-06 13:03:04 -0500

    challenge from class twenty-five

--- src/components/CreateTodoButton.js
+++ src/components/CreateTodoButton.js
@@ -1,6 +1,19 @@
+import React from "react";
 import "../styles/CreateTodoButton.css";
+import { TodoContext } from "./shared/TodoContext";
+
 const CreateTodoButton = () => {
-  return <button className="create-todo-button">+</button>;
+  const { isOpenModal, setIsOpenModal } = React.useContext(TodoContext);
+
+  const createNewTask = () => {
+    setIsOpenModal(!isOpenModal);
+  };
+
+  return (
+    <button className="create-todo-button" onClick={createNewTask}>
+      +
+    </button>
+  );
 };
 
 export { CreateTodoButton };
